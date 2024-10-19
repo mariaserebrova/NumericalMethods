19ef20cf17e39fed3065308f2d944375bedd13fb
mariaserebrova <maria.serebrova@gmail.com>
2024-10-19 17:14:16 +0300

    packages added

--- integral/main.py
+++ integral/main.py
@@ -13,46 +13,6 @@ def p(x, a=2.5, b=3.3, alpha=2/3, beta=0):
 def F(x):
     return f(x) * p(x)
 
-def left_rectangular(a, b, n):
-    h = (b - a) / n
-    res = 0
-
-    for i in range(n):
-        res += ((a + h * (i + 1)) - (a + h * i)) * f(a + h * i)
-
-    return res
-
-def middle_rectangular(a, b, n):
-    h = (b - a) / n
-    res = 0
-
-    for i in range(n):
-        mid_point = (a + h * i) + h / 2
-        res += f(mid_point) * h
-    return res
-
-def trapezoid(a, b, n):
-    h = (b - a) / n
-    res = 0
-
-    for i in range(n):
-        res += (h / 2) * (f(a + h * i) + f(a + h * (i + 1)))
-
-    return res
-
-def simpson(a, b, n):
-   h = (b - a) / (2 * n)
-
-   res = f(a) + f(b)
-
-   for i in range(1, 2 * n):
-       if i % 2 != 0:
-           res += 4 * f(a + i * h)
-       else:
-           res += 2 * f(a + i * h)
-
-   return res * h / 3
-
 def calculate_exact_integral(a, b):
     result, _ = quad(f, a, b)  # Используем quad из scipy для вычисления интеграла
     return result
